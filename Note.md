eaf0476b293274c6108165dfbec59a7947005153
seguna15 <seguna15@gmail.com>
2025-04-01 17:42:46 +0100

    route handlers

---
+++ Note.md
@@ -0,0 +1,8 @@
+# Route handlers
+
+- Custom request route handler
+- For Restful endpoints
+- No need for separate servers
+- Methods [GET, POST, PUT, PATCH, DELETE, HEAD & OPTIONS]
+- 405 is returned for unsupported response
+- Always watch out for conflict between route handler and page route. To fix create an api subdirectory
---
+++ src/app/comments/[id]/route.ts
@@ -0,0 +1,36 @@
+import { comments } from "../data";
+
+export async function GET(_request: Request, {params}: {params: Promise<{id: string}>}){
+    const {id} = await params;
+    const comment = comments.find(comment => comment.id === parseInt(id))
+    
+    return new Response(JSON.stringify(comment))
+}
+
+export async function PATCH(request: Request, {params}: {params: Promise<{id: string}>}){
+     const { id } = await params;
+     const body = await request.json();
+     const {text} = body
+     const index = comments.findIndex((comment) => comment.id === parseInt(id));
+
+     comments[index].text = text;
+
+     return new Response(JSON.stringify(comments[index]));
+}
+
+export async function DELETE(
+  _request: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
+ 
+ 
+  const index = comments.findIndex((comment) => comment.id === parseInt(id));
+
+  const deletedComment = comments[index]
+  comments.splice(index, 1);
+
+  return Response.json(deletedComment);
+  
+}
+
